494eda07f032617465d8e932d7328105f84d8c9c
santi <josevegasantiago8919@gmail.com>
2020-11-17 12:33:48 -0300

    Elimine la entidad deporte y reformule en la capa de negocio

--- CapaNegocio/Negocio_Deporte.cs
+++ CapaNegocio/Negocio_Deporte.cs
@@ -11,8 +11,26 @@ namespace CapaNegocio
 {
     public class Negocio_Deporte
     {
+
+        private int idDeporte;
+        private string Codigo;
+        private string Nombre;
+        private string Dias;
+        private string Horarios;
+        private int idProfesor;
+        private string Buscar;
+        Negocio_Profesor profe = new Negocio_Profesor();
+
         Datos_Deporte datos_Deporte = new Datos_Deporte();
-        Entidades_Deporte entidades_Deporte = new Entidades_Deporte();
+
+        public int IdDeporte { get => idDeporte; set => idDeporte = value; }
+        public string Codigo1 { get => Codigo; set => Codigo = value; }
+        public string Nombre1 { get => Nombre; set => Nombre = value; }
+        public string Dias1 { get => Dias; set => Dias = value; }
+        public string Horarios1 { get => Horarios; set => Horarios = value; }
+        public int IdProfesor { get => idProfesor; set => idProfesor = value; }
+        public string Buscar1 { get => Buscar; set => Buscar = value; }
+        public Negocio_Profesor Profe { get => profe; set => profe = value; }
 
         public DataTable ListarDeporte()
         {
